ea730ccf7642980749da49b14873c81b4b6c1705
balashankar1180 <balashankar1180@gmail.com>
2022-06-27 15:17:51 +0530

    Addede Routing between components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student/student.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { component : StudentComponent, path : "student" }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
