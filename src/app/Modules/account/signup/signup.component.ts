3871f1521e00042768c0be0d52a448667592d9f5
balashankar1180 <balashankar1180@gmail.com>
2022-07-27 17:31:45 +0530

    forms module

--- src/app/Modules/account/signup/signup.component.ts
+++ src/app/Modules/account/signup/signup.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -11,18 +12,34 @@ import { FormsModule } from '@angular/forms';
 export class SignupComponent implements OnInit {
   isDirty=true;
   name = new FormControl('');
+
+  userForm: FormGroup;
+  
+  
   updateName() {
   this.name.setValue('Nancy');
   }
-  userForm = new FormGroup({
-    fName: new FormControl(''),
-    lName: new FormControl(''),
-    email: new FormControl(''),
-    pswd: new FormControl('')
-  });
+  
+  addSkillButtonClick():void{
+    (<FormArray>this.userForm.get("skills")).push(this.addfewSkill());
+    
+  }
+
+
+
+  
+
+  addfewSkill():FormGroup
+  {
+    return new FormGroup({
+        skillName: new FormControl(''),
+        profeciency:new FormControl('') 
+       })
+  }
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.log(this.userForm.value);
+    console.log((<FormArray>this.userForm.get("skills")).controls);
     this.userForm.reset();
   }
 
@@ -34,6 +51,37 @@ export class SignupComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.userForm = new FormGroup({
+      fName: new FormControl('',[Validators.required,Validators.minLength(10)]),
+      lName: new FormControl(''),
+      email: new FormControl('',[Validators.required, Validators.email, this.validateEmail]),
+      pswd: new FormControl(''),
+      skills : new FormArray([
+        this.addfewSkill()
+        
+        // new FormGroup({
+        //   skillName: new FormControl(''),
+        //   profeciency:new FormControl('') 
+        // })
+          
+  
+      ])
+      
+  
+    });
+  
+  }
+
+
+  validateEmail(control: AbstractControl): { [key: string]: any } | null {
+    const email: string = control.value;
+    const domain = email != null ?email.substring(email.lastIndexOf('@') + 1): '';
+    if (email === '' || domain.toLowerCase() === 'scoriait.com') {
+      return null;
+    } else {
+      return { 'validateEmail': true };
+      
+    }
   }
 
 }
