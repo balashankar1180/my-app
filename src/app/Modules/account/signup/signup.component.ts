02c976d04e2ac484d82c6e1c0f9fcc01750d3abc
balashankar1180 <balashankar1180@gmail.com>
2022-07-30 00:13:50 +0530

    hjh

--- src/app/Modules/account/signup/signup.component.ts
+++ src/app/Modules/account/signup/signup.component.ts
@@ -19,41 +19,35 @@ export class SignupComponent implements OnInit {
   Instantiation of FormGroup, passing the collection of child/form controls as a first argument.
   The key for each child registers is the name for the control.
   */
- 
-  // this.userForm = new FormGroup({
-  // fName : new FormControl('',[Validators.required,Validators.minLength(10)]),
-  // lName: new FormControl(''),
-  // email: new FormControl('',[Validators.required, Validators.email, CustomValidators.validateEmail]),//this.validateEmail]),
-  // pswd: new FormControl(''),
-  // skills : new FormArray([ this.addfewSkill() ])
-  // });
-
-
-
+  /*
+  this.userForm = new FormGroup({
+  fName : new FormControl('',[Validators.required,Validators.minLength(10)]),
+  lName: new FormControl(''),
+  email: new FormControl('',[Validators.required, Validators.email, CustomValidators.validateEmail]),//this.validateEmail]),
+  pswd: new FormControl(''),
+  skills : new FormArray([ this.addfewSkill() ])
+  });
+  */
   this.userForm = this.x.group({
-    fName :['',[Validators.required,Validators.minLength(10)]],
-    lName: [''],
-    email: ['',[Validators.required, Validators.email, CustomValidators.validateEmail]],//this.validateEmail]),
-    pswd: [''],
-    skills : this.x.array([ this.addfewSkill() ])
+  fName : ['', [Validators.required, Validators.minLength(5)]],
+  lName : ['', [Validators.required, Validators.minLength(5)]],
+  email : ['', [Validators.required, Validators.email, CustomValidators.validateEmail]],
+  pswd  : ['', [Validators.required, Validators.minLength(5)]],
+  skills : this.x.array([ this.addfewSkill() ])
     });
-
-
   }
 
   addfewSkill() : FormGroup // Method contains array of two controls skillName and proficiency
   {
   return new FormGroup({
-  skillName : new FormControl('',[Validators.required]),
-  profeciency : new FormControl('',[Validators.required]) 
+  skillName : new FormControl('', [Validators.required]),
+  profeciency : new FormControl('', [Validators.required]) 
   })
   }
-
   addSkillButtonClick() : void // Method to call when button is clicked to add one more set of skills
   {
   (<FormArray>this.userForm.get("skills")).push(this.addfewSkill());
   }
-
   onSubmit()
   {
   console.log(this.userForm.value); // To log Form values to console
